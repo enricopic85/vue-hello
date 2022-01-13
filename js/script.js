a89ccc85e9aed63590c5bda3d57b892fda3d941a
enricopic85 <enricodev85@gmail.com>
2022-01-13 17:21:58 +0100

    prima lettera nome

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ new Vue({
         src:'https://media-assets.ad-italia.it/photos/60fe88ac74e20fc41794f497/16:9/w_1280,c_limit/Sudafrica_fynbos_1.jpg',
         name:'',
         listaNomi:[],
-        nameLog:''
+        nameLog:'',
+        iniziale:''
     },
     methods:{
         saluta:function(){
@@ -13,6 +14,12 @@ new Vue({
         },
         pushNomi:function(){
             this.listaNomi.push(this.name.toLowerCase())
+            console.log(this.listaNomi)
         },
+        filtraNomi:function(){
+             iniziale=this.listaNomi.filter(function(element){
+                return element[0]===name[0].toLowerCase();
+            })
+        }
     }
 })
