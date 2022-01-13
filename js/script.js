8c0e1ffb4980f06bdcec8d58e163d9ab60dbc4d0
enricopic85 <enricodev85@gmail.com>
2022-01-13 16:35:23 +0100

    tolowercase

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ new Vue({
         message:"Hello Vue",
         src:'https://media-assets.ad-italia.it/photos/60fe88ac74e20fc41794f497/16:9/w_1280,c_limit/Sudafrica_fynbos_1.jpg',
         name:'',
-        listaNomi:[]
+        listaNomi:[],
+        nameLog:''
     },
     methods:{
         saluta:function(){
             alert( "ciao " + this.name)
         },
         pushNomi:function(){
-            this.listaNomi.push(this.name)
-            console.log(this.listaNomi)
-        }
+            this.listaNomi.push(this.name.toLowerCase())
+        },
     }
 })
